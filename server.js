44e9a182f6eac1b7535396e8df47d082fd12fd9d
Oshenth Jayathilake <oshenth@gmail.com>
2026-02-04 00:57:31 +0530

    APIs added

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.get("/api/notes", (req, res) => {
   res.status(200).send("You got 450 notes");
 });
 
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Note created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note updated successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note deleted successfully!" });
+});
+
 app.listen(5001, () => {
   console.log("Server started on PORT: 5001");
 });
